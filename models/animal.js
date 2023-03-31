79b890d43f56ae131bf87b4c8fa092bddfe698ee
jaugs <johnaugsburger@gmail.com>
2023-03-30 17:50:47 -0700

    initial

---
+++ models/animal.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AnimalSchema = new Schema({
+  name: { type: String, required: true },
+  scientificname: { type: String, required: true },
+  current_version: { type: String, required: true },
+  description: { type: String, required: false },
+  diet:  {
+    type: String,
+    required: true,
+    enum: ["Omnivore", "Carnivore", "Herbivore"],
+    default: "Omnivore",
+  },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  synth_date: { type: Date, default: Date.now }
+});
+
+// Virtual for animals's URL
+AnimalSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/animals/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Animal", AnimalSchema);
